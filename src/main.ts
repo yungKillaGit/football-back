6a7e5e748824b73243adf93ea5c0f9cf5b29fe47
Artur <q79199467455q@gmail.com>
2022-04-23 08:07:53 +0300

    Add global interceptor which map responses

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { MapResponseInterceptor } from 'common/interceptors/map-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,6 +14,8 @@ async function bootstrap() {
 
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new MapResponseInterceptor());
+
   await app.listen(port);
 
   console.log(`App init on http://localhost:${port}`);
